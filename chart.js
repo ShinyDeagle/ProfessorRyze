24f672c799b258e5025ee1779fce4bbd897b971c
Rifle D. Luffy <theproplayer2@gmail.com>
2018-06-12 23:10:38 +0300

    Formatting Fixes

--- chart.js
+++ chart.js
@@ -8,262 +8,261 @@ var establishedDate = "Fri Jul 14 2017";
 
 let chartData = JSON.parse(fs.readFileSync('./chartData.json', 'utf8'));
 
-
 function writeChartData() {
-	fs.writeFile("./chartData.json", JSON.stringify(chartData), (err) => {
-		if (err) console.error(err)
-	});
+  fs.writeFile("./chartData.json", JSON.stringify(chartData), (err) => {
+    if (err) console.error(err)
+  });
 }
 
 function getCurrentDate() {
-	var date;
-	var now = new Date(Date.now()).toDateString();
-	if (chartData.currentdate == now) return chartData.currentdate;
-	else {
-		chartData.currentdate = now;
-		return now;
-	}
+  var date;
+  var now = new Date(Date.now()).toDateString();
+  if (chartData.currentdate == now) return chartData.currentdate;
+  else {
+    chartData.currentdate = now;
+    return now;
+  }
 }
 
 function getDateStringBefore(dateString) {
-	var date = new Date(dateString);
-	var yesterday = new Date(date.getTime());
-	yesterday.setDate(date.getDate() - 1);
-	return yesterday.toDateString();
+  var date = new Date(dateString);
+  var yesterday = new Date(date.getTime());
+  yesterday.setDate(date.getDate() - 1);
+  return yesterday.toDateString();
 }
 
 function getDatesInRange(timescale, count) {
 
-	var dates = [];
-	var now = new Date();
+  var dates = [];
+  var now = new Date();
 
-	switch (timescale) {
-		case "day":
-		case "days":
-		case "d":
-			for (i = 0; i < count; i++) {
-				if (i == 0) dates.push(now.toDateString());
-				else {
-					var newDate = getDateStringBefore(dates[i - 1]);
-					if (newDate == establishedDate) break;
-					else dates.push(newDate);
-				}
-			}
-			break;
+  switch (timescale) {
+    case "day":
+    case "days":
+    case "d":
+      for (i = 0; i < count; i++) {
+        if (i == 0) dates.push(now.toDateString());
+        else {
+          var newDate = getDateStringBefore(dates[i - 1]);
+          if (newDate == establishedDate) break;
+          else dates.push(newDate);
+        }
+      }
+      break;
 
-		case "week":
-		case "weeks":
-		case "wk":
-		case "w":
-			for (i = 0; i < count * 7; i++) {
-				if (i == 0) dates.push(now.toDateString());
-				else {
-					var newDate = getDateStringBefore(dates[i - 1]);
-					if (newDate == establishedDate) break;
-					else dates.push(newDate);
-				}
-			}
-			break;
+    case "week":
+    case "weeks":
+    case "wk":
+    case "w":
+      for (i = 0; i < count * 7; i++) {
+        if (i == 0) dates.push(now.toDateString());
+        else {
+          var newDate = getDateStringBefore(dates[i - 1]);
+          if (newDate == establishedDate) break;
+          else dates.push(newDate);
+        }
+      }
+      break;
 
-		case "month":
-		case "months":
-		case "m":
-			for (i = 0; i < count * 31; i++) {
-				if (i == 0) dates.push(now.toDateString());
-				else {
-					var newDate = getDateStringBefore(dates[i - 1]);
-					if (newDate == establishedDate) break;
-					else dates.push(newDate);
-				}
-			}
-			break;
+    case "month":
+    case "months":
+    case "m":
+      for (i = 0; i < count * 31; i++) {
+        if (i == 0) dates.push(now.toDateString());
+        else {
+          var newDate = getDateStringBefore(dates[i - 1]);
+          if (newDate == establishedDate) break;
+          else dates.push(newDate);
+        }
+      }
+      break;
 
-		case "year":
-		case "years":
-		case "y":
-			for (i = 0; i < count * 365; i++) {
-				if (i == 0) dates.push(now.toDateString());
-				else {
-					var newDate = getDateStringBefore(dates[i - 1]);
-					if (newDate == establishedDate) break;
-					else dates.push(newDate);
-				}
-			}
-			break;
-	}
+    case "year":
+    case "years":
+    case "y":
+      for (i = 0; i < count * 365; i++) {
+        if (i == 0) dates.push(now.toDateString());
+        else {
+          var newDate = getDateStringBefore(dates[i - 1]);
+          if (newDate == establishedDate) break;
+          else dates.push(newDate);
+        }
+      }
+      break;
+  }
 
-	return dates;
+  return dates;
 }
 
 function getUserDateData(dateArray) {
 
-	var data = [];
+  var data = [];
 
-	for (i = 0; i < dateArray.length; i++) {
-		if (!chartData.dates[dateArray[dateArray.length - 1 - i]]) continue;
-		data.push(chartData.dates[dateArray[dateArray.length - 1 - i]].users);
-	}
-	return data;
+  for (i = 0; i < dateArray.length; i++) {
+    if (!chartData.dates[dateArray[dateArray.length - 1 - i]]) continue;
+    data.push(chartData.dates[dateArray[dateArray.length - 1 - i]].users);
+  }
+  return data;
 }
 
 function backupGuildData() {
-	fs.writeFile(`./chartDataBackups/${getCurrentDate()}.json`, JSON.stringify(chartData), (err) => {
-		if (err) console.error(err)
-	});
+  fs.writeFile(`./chartDataBackups/${getCurrentDate()}.json`, JSON.stringify(chartData), (err) => {
+    if (err) console.error(err)
+  });
 }
 
 module.exports = {
 
-	updateGuildCount: (count) => {
-		if (!chartData.dates[getCurrentDate()]) chartData.dates[getCurrentDate()] = {
-			users: count
-		}
-		chartData.dates[getCurrentDate()].users = count;
-		writeChartData();
-		backupGuildData();
-	},
+  updateGuildCount: (count) => {
+    if (!chartData.dates[getCurrentDate()]) chartData.dates[getCurrentDate()] = {
+      users: count
+    }
+    chartData.dates[getCurrentDate()].users = count;
+    writeChartData();
+    backupGuildData();
+  },
 
-	//Currently inactive since data has already been extrapolated.
-	//Try not to call it at any point
-	extrapolateCount: () => {
+  //Currently inactive since data has already been extrapolated.
+  //Try not to call it at any point
+  extrapolateCount: () => {
 
-		var intialCount = chartData.dates[getCurrentDate()].users;
+    var intialCount = chartData.dates[getCurrentDate()].users;
 
-		var finalDay = "Fri Jul 14 2017"
-		var extrapolatedDay = getCurrentDate();
-		var extrapolatedCount = chartData.dates[extrapolatedDay].users;
-		var countBefore = extrapolatedCount;
+    var finalDay = "Fri Jul 14 2017"
+    var extrapolatedDay = getCurrentDate();
+    var extrapolatedCount = chartData.dates[extrapolatedDay].users;
+    var countBefore = extrapolatedCount;
 
-		var i = 0;
-		while (extrapolatedDay != finalDay) {
-			if (!chartData.dates[extrapolatedDay]) {
-				if (countBefore < 10) break;
-				var newCount = countBefore - 1;
-				if (i == 3 || i == 5) newCount--;
-				if (i == 10) i = 0;
-				i++;
-				chartData.dates[extrapolatedDay] = {
-					users: newCount
-				}
-				countBefore = chartData.dates[extrapolatedDay].users;
-				extrapolatedDay = getDateStringBefore(extrapolatedDay);
-			} else {
-				extrapolatedDay = getDateStringBefore(extrapolatedDay);
-				countBefore = extrapolatedCount;
-				console.log(countBefore);
-				if (chartData.dates[extrapolatedDay]) extrapolatedCount = chartData.dates[extrapolatedDay].users;
-				continue;
-			}
-		}
-		writeChartData();
-	},
+    var i = 0;
+    while (extrapolatedDay != finalDay) {
+      if (!chartData.dates[extrapolatedDay]) {
+        if (countBefore < 10) break;
+        var newCount = countBefore - 1;
+        if (i == 3 || i == 5) newCount--;
+        if (i == 10) i = 0;
+        i++;
+        chartData.dates[extrapolatedDay] = {
+          users: newCount
+        }
+        countBefore = chartData.dates[extrapolatedDay].users;
+        extrapolatedDay = getDateStringBefore(extrapolatedDay);
+      } else {
+        extrapolatedDay = getDateStringBefore(extrapolatedDay);
+        countBefore = extrapolatedCount;
+        console.log(countBefore);
+        if (chartData.dates[extrapolatedDay]) extrapolatedCount = chartData.dates[extrapolatedDay].users;
+        continue;
+      }
+    }
+    writeChartData();
+  },
 
-	createChart: (client, message, args) => {
+  createChart: (client, message, args) => {
 
-		var range = "";
+    var range = "";
 
-		if (!args[1] || !args[2]) return;
-		
-		var timescale = args[1].toLowerCase();
-		var limit = parseInt(args[2].toLowerCase());
+    if (!args[1] || !args[2]) return;
 
-		var dates = getDatesInRange(timescale, limit);
-		var dateData = getUserDateData(dates);
+    var timescale = args[1].toLowerCase();
+    var limit = parseInt(args[2].toLowerCase());
 
-		dates = dates.reverse();
+    var dates = getDatesInRange(timescale, limit);
+    var dateData = getUserDateData(dates);
 
-		for (i = 0; i < dates.length; i++) {
-			if (!chartData.dates[dates[i]]) {
-				dates.splice(i,1);
-				i--;
-			}
-		}
+    dates = dates.reverse();
 
-		var dateArray = [];
+    for (i = 0; i < dates.length; i++) {
+      if (!chartData.dates[dates[i]]) {
+        dates.splice(i, 1);
+        i--;
+      }
+    }
 
-		for (i = 0; i < dates.length; i++) dateArray.push(`${dates[i]}: ${dateData[i]} users`)
+    var dateArray = [];
 
-		//Change
-		var initialCount = dateData[0];
-		var finalCount = dateData[dateData.length - 1];
+    for (i = 0; i < dates.length; i++) dateArray.push(`${dates[i]}: ${dateData[i]} users`)
 
-		var changePercent = (((finalCount - initialCount) / initialCount) * 100).toFixed(2);
+    //Change
+    var initialCount = dateData[0];
+    var finalCount = dateData[dateData.length - 1];
 
-		if (changePercent < 0) {
-			suffix = "decrease"
-			changePercent = 0 - changePercent;
-		} else if (changePercent == 0){
-			suffix = ""
-		}	else {
-			suffix = "increase"
-		}
+    var changePercent = (((finalCount - initialCount) / initialCount) * 100).toFixed(2);
 
-		var change = "";
+    if (changePercent < 0) {
+      suffix = "decrease"
+      changePercent = 0 - changePercent;
+    } else if (changePercent == 0) {
+      suffix = ""
+    } else {
+      suffix = "increase"
+    }
 
-		switch (suffix) {
-			case "decrease":
-				change = `There has been a ${changePercent}% [${finalCount - initialCount} users] decrease in user count`;
-				break;
-			case "increase":
-				change = `There has been a ${changePercent}% [${finalCount - initialCount} users] increase in user count`;
-				break;
-			default:
-				change = `There has been no increase or decrease in user count`;
-				break;
-		}
+    var change = "";
 
-		//Highs and Lows
+    switch (suffix) {
+      case "decrease":
+        change = `There has been a ${changePercent}% [${finalCount - initialCount} users] decrease in user count`;
+        break;
+      case "increase":
+        change = `There has been a ${changePercent}% [${finalCount - initialCount} users] increase in user count`;
+        break;
+      default:
+        change = `There has been no increase or decrease in user count`;
+        break;
+    }
 
-		var highDay = [];
-		var lowDay = [];
+    //Highs and Lows
 
-		var lowestCount = 1000000000000000000000000000;
-		var highestCount = -lowestCount;
-		var index = 0;
+    var highDay = [];
+    var lowDay = [];
 
-		for (i = 0; i < dateData.length; i++) {
-			var count = dateData[i];
-			if (count > highestCount) {
-				highestCount = count;
-				index = i;
-			}
-		}
+    var lowestCount = 1000000000000000000000000000;
+    var highestCount = -lowestCount;
+    var index = 0;
 
-		highDay[0] = dates[index];
-		highDay[1] = dateData[index]
+    for (i = 0; i < dateData.length; i++) {
+      var count = dateData[i];
+      if (count > highestCount) {
+        highestCount = count;
+        index = i;
+      }
+    }
 
-		for (i = 0; i < dateData.length; i++) {
-			var count = dateData[i];
-			if (count < lowestCount) {
-				lowestCount = count;
-				index = i;
-			}
-		}
+    highDay[0] = dates[index];
+    highDay[1] = dateData[index]
 
-		lowDay[0] = dates[index];
-		lowDay[1] = dateData[index];
+    for (i = 0; i < dateData.length; i++) {
+      var count = dateData[i];
+      if (count < lowestCount) {
+        lowestCount = count;
+        index = i;
+      }
+    }
 
-		var sizeOfField = 7;
-		var totalFields = Math.ceil(dateArray.length / sizeOfField);
+    lowDay[0] = dates[index];
+    lowDay[1] = dateData[index];
 
-		//The embed breaks if I have more than 24 fields. I'm using 2 already so I can also use 22.
-		if (totalFields > 22) totalFields = 22;
+    var sizeOfField = 7;
+    var totalFields = Math.ceil(dateArray.length / sizeOfField);
 
-		var weekChart = new Discord.RichEmbed()
-			.setTitle(`User Count in a ${limit} ${timescale} period`)
-			.setColor(092030)
-			.setThumbnail(message.author.avatarURL)
-			for (i = 0; i < totalFields; i++) {
-				var tempArray = [];
-				for (j = 0 + i * sizeOfField; j < sizeOfField + i * sizeOfField; j++) {
-					tempArray.push(dateArray[j]);
-				}
-				weekChart.addField(`Week ${i}`, tempArray, true);
-			}
-			weekChart.addField("Statistics",`${change}\nThe date with the most users [${highDay[1]}] was ${highDay[0]}\nThe date with the least users [${lowDay[1]}] was ${lowDay[0]}`, true)
-			weekChart.setFooter("Created and Currently Maintained by Rifle D. Luffy#1852 from the Official MS Discord.", "https://i.imgur.com/zEOYDNJ.png")
-			weekChart.addField("Notes","Dates before May 24 2018 have been extrapolated")
-		message.channel.send(weekChart);
-	}
+    //The embed breaks if I have more than 24 fields. I'm using 2 already so I can also use 22.
+    if (totalFields > 22) totalFields = 22;
+
+    var weekChart = new Discord.RichEmbed()
+      .setTitle(`User Count in a ${limit} ${timescale} period`)
+      .setColor(092030)
+      .setThumbnail(message.author.avatarURL)
+    for (i = 0; i < totalFields; i++) {
+      var tempArray = [];
+      for (j = 0 + i * sizeOfField; j < sizeOfField + i * sizeOfField; j++) {
+        tempArray.push(dateArray[j]);
+      }
+      weekChart.addField(`Week ${i}`, tempArray, true);
+    }
+    weekChart.addField("Statistics", `${change}\nThe date with the most users [${highDay[1]}] was ${highDay[0]}\nThe date with the least users [${lowDay[1]}] was ${lowDay[0]}`, true)
+    weekChart.setFooter("Created and Currently Maintained by Rifle D. Luffy#1852 from the Official MS Discord.", "https://i.imgur.com/zEOYDNJ.png")
+    weekChart.addField("Notes", "Dates before May 24 2018 have been extrapolated")
+    message.channel.send(weekChart);
+  }
 
 }
--- submit.js
+++ submit.js
@@ -3,46 +3,41 @@ const Discord = require('discord.js');
 
 var bot = new Discord.Client()
 
-module.exports= {
-	submit: (client, message, args) => {
-		switch (args[1].toLowerCase()) {
-			case "spell":
-				if (args[2] != "") reason = message.content.slice(13);
-				else reason = "UnExplained";
+module.exports = {
+  submit: (client, message, args) => {
+    switch (args[1].toLowerCase()) {
+      case "spell":
+        if (args[2] != "") reason = message.content.slice(13);
+        else reason = "UnExplained";
 
-				message.channel.send("Waiting 5s for all files to load.")
-				delay(2000)
-					.then(() => {
-						var attachments = message.attachments
-						var filename = "";
-						var link = "";
-						var username = message.author.username
-						var id = message.author.id
+        message.channel.send("Waiting 5s for all files to load.")
+        delay(2000)
+          .then(() => {
+            var attachments = message.attachments
+            var filename = "";
+            var link = "";
+            var username = message.author.username
+            var id = message.author.id
 
-						attachments.forEach(function(attachment)
-						{
-							filename = attachment.filename
-							link = attachment.url
+            attachments.forEach(function(attachment) {
+              filename = attachment.filename
+              link = attachment.url
 
-							if (!filename.startsWith("spells-") && !filename.endsWith(".yml"))
-								{
-									message.channel.send(`${filename} is not a valid spell file!`)
-									message.react(emojiDB.react("cross"))
-								}
-							else
-								{
-									message.channel.send("Spell File Sent!")
-									client.channels.get("392961616833937408").send(`${username}/${id} sent a spell file with a reason which is: ${reason}.`)
-									message.channel.fetchMessage(message.id)
-									.then(message =>
-										{
-											client.channels.get("392961616833937408").send(link)
-										})
-								}
-						})
-						message.react(emojiDB.react("tick"))
-					})
-				break;
-		}
-	}
+              if (!filename.startsWith("spells-") && !filename.endsWith(".yml")) {
+                message.channel.send(`${filename} is not a valid spell file!`)
+                message.react(emojiDB.react("cross"))
+              } else {
+                message.channel.send("Spell File Sent!")
+                client.channels.get("392961616833937408").send(`${username}/${id} sent a spell file with a reason which is: ${reason}.`)
+                message.channel.fetchMessage(message.id)
+                  .then(message => {
+                    client.channels.get("392961616833937408").send(link)
+                  })
+              }
+            })
+            message.react(emojiDB.react("tick"))
+          })
+        break;
+    }
+  }
 }
